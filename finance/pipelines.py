11f4ddd213038850f7a38ccf491983919df08b6d
deqiang-kong <452211588@qq.com>
2018-05-01 16:56:23 +0800

    first commit

--- finance/pipelines.py
+++ finance/pipelines.py
@@ -6,8 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymysql import cursors
 
-from finance import items
-from finance.items import ColumnItem
+from finance.items import ColumnItem, ColumnDetailItem
 from finance.util.connect import connect_net
 from twisted.enterprise import adbapi
 
@@ -36,8 +35,8 @@ class FinancePipeline(object):
             print(spider.name + " 专栏数据入库............." + item['title'])
         #     store_column(item, self.conn, self.dailys)
         #
-        # elif item_name is items.column_detail:
-        #     print(spider.name + " 专栏详情入库............." + item_name)
+        elif isinstance(item, ColumnDetailItem):
+            print(spider.name + " 专栏详情入库.............")
         #     store_detail(item, self.conn, self.dailys_detail)
 
         return item
@@ -47,45 +46,3 @@ class FinancePipeline(object):
         print(spider.name + " 爬虫结束.............")
         self.conn.close()
 
-
-class JianshuTwistedPipeline(object):
-    def __init__(self):
-        dbparams = {
-            'host': '127.0.0.1',
-            'port': 3306,
-            'user': 'root',
-            'password': 'root',
-            'database': 'jianshu2',
-            'charset': 'utf8',
-            'cursorclass': cursors.DictCursor
-        }
-        # 数据库连接池
-        self.dbpool = adbapi.ConnectionPool('pymysql', **dbparams)
-        self._sql = None
-
-    @property
-    def sql(self):
-        if not self._sql:
-            self._sql = """
-                insert into article(id,title,content,author,avatar,pub_time,origin_url,article_id) values(null,%s,%s,%s,%s,%s,%s,%s)
-                """
-            return self._sql
-        return self._sql
-
-
-    def process_item(self, item, spider):
-        # 异步插入
-        defer = self.dbpool.runInteraction(self.insert_item, item)
-        defer.addErrback(self.handle_error, item, spider)
-
-    #
-    def insert_item(self, cursor, item):
-        cursor.execute(self.sql, (
-            item['title'], item['content'], item['author'], item['avatar'], item['pub_time'], item['origin_url'],
-            item['article_id']))
-
-    # 错误输出
-    def handle_error(self, error, item, spider):
-        print('=' * 10 + "error" + '=' * 10)
-        print(error)
-        print('=' * 10 + "error" + '=' * 10)
